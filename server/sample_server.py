aced8a85e5da3c91b8c996495ff3c509a7fbd980
johnp <jp@gmail.com>
2014-08-27 17:04:18 -0400

    adding search by department

--- server/sample_server.py
+++ server/sample_server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import g
+from flask import request
 import MySQLdb
 import json
 import ConfigParser
@@ -19,7 +20,11 @@ logger.addHandler(fh)
 logger.addHandler(ch)
 
 
-select_sql = '''select school_id, school_name, grouping, departement , community , city, address, telephone from schools where school_name like '{}%' LIMIT 30'''
+select_sql = '''select school_id, school_name, grouping, departement , community, 
+city, address, telephone, neighborhood, grant_suggested_by, director_name, 
+year1_effective, year2_effective, year3_effective, year4_effective, year5_effective, 
+year6_effective, unknown_effective, total_effective 
+from schools where school_name like '{}%' '''
 
 select_school_sql = '''select grouping, departement , community , city, neighborhood, grant_suggested_by, school_name,
     director_name, address, telephone, year1_effective, year2_effective, year3_effective,
@@ -46,6 +51,20 @@ def db_connect():
 def db_disconnect(exception=None):
     g.db_conn.close()
 
+
+@app.route('/departments')
+def get_departments():
+    cur = g.db_conn.cursor()
+    sql = "select distinct(departement) from schools where  departement != '' order by 1"
+    logger.info("executing: {0}".format(sql))
+    cur.execute(sql)
+    departments = []
+    for row in cur.fetchall():
+        departments.append({'name': row[0]})
+
+    return 'departments_data_callback( {0} )'.format(json.dumps(departments, ensure_ascii=False))
+
+
 @app.route('/school/<school_id>')
 def get_school_data(school_id):
     cur = g.db_conn.cursor()
@@ -61,15 +80,54 @@ def get_school_data(school_id):
     logger.info("scholl data: {0}".format(json.dumps(school_details)))
     return 'school_data_callback( {0} )'.format(json.dumps(school_details))
 
-@app.route('/matches/<school_name>')
-def get_matching_schools(school_name):
+
+
+
+
+#@app.route('/matches/<school_name>')
+
+@app.route('/matches')
+def get_matching_schools():
     cur = g.db_conn.cursor()
+    school_name = request.args.get('school_name', None)
+
+    if school_name is None:
+        school_name = ''
+
+    department = request.args.get('department', None)
+
     sql = select_sql.format(school_name)
+
+    if department  is not  None:
+        sql += " and departement = '{}'".format(department)
+
+    sql +=' LIMIT 30'
+
     logger.info("executing: {0}".format(sql))
     cur.execute(sql)
     schools = []
     for row in cur.fetchall():
-        schools.append({'id': row[0], 'name': row[1] , 'grouping': row[2], 'departement': row[3], 'community': row[4], 'city': row[5], 'address': row[6], 'telephone': row[7] })
+        schools.append({
+            'id': row[0], 
+            'name': row[1], 
+            'grouping': row[2], 
+            'department': row[3], 
+            'community': row[4], 
+            'city': row[5], 
+            'address': row[6], 
+            'telephone': row[7],
+            'neighborhood': row[8],
+            'grant_suggested_by': row[9], 
+            'director_name': row[10], 
+            'year1_effective': row[11], 
+            'year2_effective': row[12], 
+            'year3_effective': row[13], 
+            'year4_effective': row[14], 
+            'year5_effective': row[15], 
+            'year6_effective': row[16], 
+            'unknown_effective': row[17], 
+            'total_effective': row[18]
+        })
 
     return 'mycallback( {0} )'.format(json.dumps(schools))
 
